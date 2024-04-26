573d8c539e19364c1ddc38bc550ac33be849ef13
Rahemb <143782157+Rahemb@users.noreply.github.com>
2024-04-26 13:06:38 +0200

    Update BillettContoller.java

--- src/main/java/com/example/obligatorisk3data1700/BillettContoller.java
+++ src/main/java/com/example/obligatorisk3data1700/BillettContoller.java
@@ -1,32 +1,35 @@
 // BillettController.java
 package com.example.obligatorisk3data1700;
 
+
+// BillettController.java
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @RestController
 public class BillettContoller {
 
     @Autowired
     private BillettRepository rep;
 
+    // Legg til en billett
     @PostMapping("/lagre")
-    public void lagreBillett(Billett innBillett) {
-        rep.lagreBillett(innBillett);
+    public void leggTilBillett(Billett billett) {
+        rep.lagreBillett(billett);
     }
 
-    @GetMapping("/HentAlle")
-    public List<Billett> hentAlle() {
-        return rep.hentAlleBilletter();
+    // Hent alle billetter
+    @GetMapping("/hentAlle")
+    public List<Billett> hentAlleBilletter() {
+        return rep.hentAlleKunder();
     }
 
-    @GetMapping("/SlettAlle")
-    public void slettAlle() {
+    // Slett alle billetter
+    @GetMapping("/slettAlle")
+    public void slettAlleBilletter() {
         rep.slettAlleBilletter();
     }
-
 }
