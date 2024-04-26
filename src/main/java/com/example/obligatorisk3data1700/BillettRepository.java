1fb7f69212e6598dfd0d67979ae57c6b504d8920
Rahemb <143782157+Rahemb@users.noreply.github.com>
2024-04-26 13:06:59 +0200

    Update BillettRepository.java

--- src/main/java/com/example/obligatorisk3data1700/BillettRepository.java
+++ src/main/java/com/example/obligatorisk3data1700/BillettRepository.java
@@ -1,6 +1,7 @@
 // BillettRepository.java
 package com.example.obligatorisk3data1700;
 
+import org.aspectj.weaver.patterns.KindedPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,20 +16,16 @@ public class BillettRepository {
     private JdbcTemplate db;
 
     public void lagreBillett(Billett innBillett){
-        String sql = "INSERT INTO Billett (film, antall_billetter) VALUES(?,?)";
-        db.update(sql, innBillett.getFilm(), innBillett.getAntallBilletter());
+        String sql = "INSERT INTO Billett (film, antall, fornavn, etternavn, telefonnummer, epost) VALUES (?,?,?,?,?,?)";
+        db.update(sql,innBillett.getFilm(),innBillett.getAntall(),innBillett.getFornavn(),innBillett.getEtternavn(),innBillett.getTelefonnummer(),innBillett.getEpost());
     }
-
-    public List<Billett> hentAlleBilletter(){
+    public List<Billett> hentAlleKunder(){
         String sql = "SELECT * FROM Billett";
-        List<Billett> alleBilletter = db.query(sql, new BeanPropertyRowMapper<>(Billett.class));
+        List<Billett> alleBilletter = db.query(sql,new  BeanPropertyRowMapper(Billett.class));
         return alleBilletter;
     }
-
     public void slettAlleBilletter(){
         String sql = "DELETE FROM Billett";
         db.update(sql);
     }
-
-
 }
