2a67abf85704f42f475a9614cc3b9089e4e5d756
Rahemb <143782157+Rahemb@users.noreply.github.com>
2024-04-26 13:06:19 +0200

    Update Billett.java

--- src/main/java/com/example/obligatorisk3data1700/Billett.java
+++ src/main/java/com/example/obligatorisk3data1700/Billett.java
@@ -1,20 +1,72 @@
 // Bilett.java
 package com.example.obligatorisk3data1700;
 
+
+// Billett.java
 public class Billett {
     private String film;
-    private String antallBilletter;
+    private int antall;
+    private String fornavn;
+    private String etternavn;
+    private String telefonnummer;
+    private String epost;
 
-    public Billett(String film, String antallBilletter){
+    // Konstruktør
+    public Billett(String film, int antall, String fornavn, String etternavn, String telefonnummer, String epost) {
         this.film = film;
-        this.antallBilletter = antallBilletter;
+        this.antall = antall;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.telefonnummer = telefonnummer;
+        this.epost = epost;
     }
-    public void Bilett(){
 
+    // Getters og setters
+    public String getFilm() {
+        return film;
     }
 
-    public String getFilm() {return film;}
-    public void setFilm(String film) {this.film = film;}
-    public String getAntallBilletter(){return antallBilletter;}
-    public void setAntallBilletter(String antallBilletter) {this.antallBilletter = antallBilletter;}
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public int getAntall() {
+        return antall;
+    }
+
+    public void setAntall(int antall) {
+        this.antall = antall;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(String telefonnummer) {
+        this.telefonnummer = telefonnummer;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
 }
